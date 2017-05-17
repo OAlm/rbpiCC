1e0f5f50c9b1da541571c4a1d4b6afdaaaad99a5
Nikita Korhonen <nikitak@metropolia.fi>
2017-05-17 14:37:24 +0300

    initial commit

---
+++ routes/api.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+var uws = require('uws');
+var defaults = require("../defaultSettings.json");
+/* GET users listing. */
+router.get('/test', function (req, res, next) {
+
+    res.status(200).send("OK!");
+});
+
+router.get('/broadcast', function (req, res) {
+    global.uwsServer.broadcast("BROADCAST KEKEKEK!");
+    res.status(200).send("OK!");
+});
+router.get('/gstream/:method', function (req, res) {
+    var id = req.query.id;
+
+    var data = {
+        proto: "gstream",
+        method: req.params.method || defaults.gstream.method,
+        sink: req.query.sink || defaults.gstream.sink,
+        port: req.query.port || defaults.gstream.port,
+        resolution: req.query.resolution || defaults.gstream.resolution
+    };
+    if (id === null) {
+        res.status(422).send("id is missing");
+    } else {
+        global.uwsServer.broadcast(JSON.stringify(data));
+        res.status(200).send("OK!");
+    }
+});
+router.get('/wowza/:method', function (req, res) {
+    var id = req.query.id;
+    console.log(req.params.method);
+    var data = {
+        proto: "wowza",
+        method: req.params.method || defaults.wowza.method,
+        host: req.query.host || defaults.wowza.host,
+        port: req.query.port || defaults.wowza.port,
+        app: req.query.app || defaults.wowza.app,
+        resolution: req.query.resolution || defaults.wowza.resolution
+    };
+    if (id === null) {
+        res.status(422).send("id is missing");
+    } else {
+        global.uwsServer.broadcast(JSON.stringify(data));
+        res.status(200).send("OK!");
+    }
+});
+module.exports = router;
---
+++ routes/socket.js
@@ -0,0 +1,28 @@
+/**
+ * Created by nikitak on 16.5.2017.
+ */
+function connection(socket){
+
+    console.log("socket module URL /", socket);
+    // You might use location.query.access_token to authenticate or share sessions
+    // or socket.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+    socket.on('message', function incoming(message) {
+
+        try{
+            var request = JSON.parse(message);
+            console.log(request.secret || "No secret");
+            console.log(request.method || "No method");
+            console.log(request.data || "No data");
+            console.log(request);
+        }catch(e){
+            console.log("JSON parse Error in socket module");
+            console.log("Error: ", e);
+            console.log("Message: ", message);
+        }
+
+    });
+
+    socket.send('Socket Module');
+};
+
+module.exports = connection;
